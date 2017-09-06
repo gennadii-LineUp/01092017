26e2285e50c6929877a8d88c76ef31adee61e5e5
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-09-06 19:01:02 +0300

    router

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from '@angular/common';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from '../services/routing.module';
-import {HeaderGeneralComponent} from './components/header/header.component';
+import {HeaderGeneralComponent} from './components/header/header-general/header.component';
 import {FooterComponent} from './components/footer/footer.component';
-import { UserMenuComponent } from './components/user-menu/user-menu.component';
+import { UserMenuComponent } from './components/header/user-menu/user-menu.component';
+import { HeaderLogedInComponent } from './components/header/header-loged-in/header-loged-in.component';
 
 
 @NgModule({
@@ -16,11 +17,13 @@ import { UserMenuComponent } from './components/user-menu/user-menu.component';
     HeaderGeneralComponent,
     FooterComponent,
     UserMenuComponent,
+    HeaderLogedInComponent,
   ],
   exports: [
     HeaderGeneralComponent,
     FooterComponent,
     UserMenuComponent,
+    HeaderLogedInComponent,
   ],
   providers: [
 
