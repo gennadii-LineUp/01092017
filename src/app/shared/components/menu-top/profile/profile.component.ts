984ea363bae16959fe171f9c587d217fd8c13293
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-09-25 14:34:24 +0300

    accordion added

--- src/app/shared/components/menu-top/profile/profile.component.ts
+++ src/app/shared/components/menu-top/profile/profile.component.ts
@@ -12,4 +12,20 @@ export class ProfileComponent implements OnInit {
   ngOnInit() {
   }
 
+  public accordionToggleItemFunction(e: any) {
+    const currentClass = e.currentTarget.className;
+    const accordionItems: NodeListOf<Element> = window.document.querySelectorAll('div.accordionItem');
+    for (let i = 0; i < accordionItems.length; i++) {
+      accordionItems[i].className = 'accordionItem close-item';
+    }
+
+    let currentAccordionItem = e.currentTarget;
+    if (currentClass === 'accordionItem close-item') {
+      currentAccordionItem.className = 'accordionItem open';
+    } else {
+      currentAccordionItem.className = 'accordionItem close-item';
+    }
+  }
+
+
 }
