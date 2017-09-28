af090887defec29c2e528a5eb85f6d9a275d4aeb
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-09-28 19:09:38 +0300

    depot-citizen page improovement

---
+++ src/app/models/receiver-class.ts
@@ -0,0 +1,14 @@
+export class ReceiverClass {
+  firstName: string;
+  lastName: string;
+  id_receiver: string;
+
+  constructor( firstName: string,
+               lastName: string,
+               id_receiver: string) {
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id_receiver = id_receiver;
+  }
+}
--- src/app/shared/components/services/depot-citizen/depot-citizen.component.ts
+++ src/app/shared/components/services/depot-citizen/depot-citizen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ReceiverClass} from '../../../../models/receiver-class';
 
 @Component({
   selector: 'app-services-depot-citizen',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./depot-citizen.component.scss']
 })
 export class DepotCitizenComponent implements OnInit {
+  newReceiver = new ReceiverClass('', '', '');
+  receiverExist = false;
+  createNewReceiver = true;
+  receiverStatus = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  public receiverExistMode() {
+    this.clearSearch();
+    this.receiverExist = true;
+  }
+  public createNewReceiverMode() {
+    this.clearSearch();
+    this.receiverStatus = 'New';
+    this.createNewReceiver = true;
+  }
+  public clearSearch() {
+    this.newReceiver = new ReceiverClass('', '', '');
+    this.receiverExist = false;
+    this.createNewReceiver = false;
+    this.receiverStatus = '';
   }
 }
