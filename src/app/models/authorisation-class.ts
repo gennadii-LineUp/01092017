17df7a10858a53352be3a96f7f01a802a7d29686
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-09-28 15:48:19 +0300

    Transfert Сompte à Compte page improvement

--- src/app/models/authorisation-class.ts
+++ src/app/models/authorisation-class.ts
@@ -1,11 +1,14 @@
 export class AuthorisationClass {
   phone: string;
   password: string;
+  mode: string;
 
   constructor( phone: string,
-               password: string) {
+               password: string,
+               mode: string) {
 
     this.phone = phone;
     this.password = password;
+    this.mode = mode;
   }
 }
--- src/app/shared/components/services/transfer-compte/transfer-compte.component.ts
+++ src/app/shared/components/services/transfer-compte/transfer-compte.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfer-compte.component.scss']
 })
 export class TransferCompteComponent implements OnInit {
+  transfer_all = true;
+  transfer_standart = false;
+  transfer_marchand = false;
+  transfer_facture = false;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  public goToAllTransferFunction() {
+    this.transfer_all = true;
+    this.transfer_standart = false;
+    this.transfer_marchand = false;
+    this.transfer_facture = false;
+  }
+  public goToStandartTransferFunction() {
+    this.transfer_all = false;
+    this.transfer_standart = true;
+    this.transfer_marchand = false;
+    this.transfer_facture = false;
+  }
+  public goToMarchandTransferFunction() {
+    this.transfer_all = false;
+    this.transfer_standart = false;
+    this.transfer_marchand = true;
+    this.transfer_facture = false;
+  }
+  public goToFactureTransferFunction() {
+    this.transfer_all = false;
+    this.transfer_standart = false;
+    this.transfer_marchand = false;
+    this.transfer_facture = true;
+  }
+
 
 }
