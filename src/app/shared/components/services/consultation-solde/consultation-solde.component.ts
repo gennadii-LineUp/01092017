cb1f62faf92b1cde66ea52d964f8763fe2647970
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-09-29 19:31:16 +0300

    page improovement: service-->consultation-solde

--- src/app/shared/components/services/consultation-solde/consultation-solde.component.ts
+++ src/app/shared/components/services/consultation-solde/consultation-solde.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {CommonServices} from '../../../../services/common.service';
+import {UserDataGlossary} from '../../../../models/user-data';
 
 @Component({
   selector: 'app-services-consultation-solde',
   templateUrl: './consultation-solde.component.html',
-  styleUrls: ['./consultation-solde.component.scss']
+  styleUrls: ['./consultation-solde.component.scss'],
+  providers: [UserDataGlossary]
 })
 export class ConsultationSoldeComponent implements OnInit {
   showRequestResult = false;
 
-  constructor(public commonServices: CommonServices) { }
+
+  constructor(public commonServices: CommonServices,
+              public userDataGlossary: UserDataGlossary) { }
 
   ngOnInit() {
   }
