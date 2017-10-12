c9aca9357ebbdd565708aa7ae80b9f24826fba2e
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-10-12 18:45:41 +0300

    page services/transfer-compte: started

--- src/app/shared/components/services/transfer-compte/transfer-compte.component.ts
+++ src/app/shared/components/services/transfer-compte/transfer-compte.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {UserDataGlossary} from '../../../../models/user-data';
+import {CommonServices} from '../../../../services/common.service';
+import {ErrorMessageHandlerService} from '../../../../services/error-message-handler.service';
+import {ReceiverClass} from '../../../../models/receiver-class';
 
 @Component({
   selector: 'app-services-transfer-compte',
   templateUrl: './transfer-compte.component.html',
-  styleUrls: ['./transfer-compte.component.scss']
+  styleUrls: ['./transfer-compte.component.scss'],
+  providers: [UserDataGlossary]
+
 })
 export class TransferCompteComponent implements OnInit {
   transfer_all = true;
@@ -11,7 +17,18 @@ export class TransferCompteComponent implements OnInit {
   transfer_marchand = false;
   transfer_facture = false;
 
-  constructor() { }
+  loading = false;
+  myAccount: any;
+  newReceiver = this.userDataGlossary.beneficiaires[0];
+  amountToReceiver: number;
+  showReceiverInfo = false;
+  successMessage = '';
+  errorMessage = '';
+
+
+  constructor(public userDataGlossary: UserDataGlossary,
+              public commonServices: CommonServices,
+              public errorMessageHandlerService: ErrorMessageHandlerService) { }
 
   ngOnInit() {
   }
@@ -29,17 +46,41 @@ export class TransferCompteComponent implements OnInit {
     this.transfer_facture = false;
   }
   public goToMarchandTransferFunction() {
-    this.transfer_all = false;
-    this.transfer_standart = false;
-    this.transfer_marchand = true;
-    this.transfer_facture = false;
+    // this.transfer_all = false;
+    // this.transfer_standart = false;
+    // this.transfer_marchand = true;
+    // this.transfer_facture = false;
   }
   public goToFactureTransferFunction() {
-    this.transfer_all = false;
-    this.transfer_standart = false;
-    this.transfer_marchand = false;
-    this.transfer_facture = true;
+    // this.transfer_all = false;
+    // this.transfer_standart = false;
+    // this.transfer_marchand = false;
+    // this.transfer_facture = true;
   }
+
+
+  public fillReceiverInfoFunction(myAccount: any, e: any) {
+    // this.showReceiverInfo = false;
+    this.clearSearch();
+    this.newReceiver = this.userDataGlossary.beneficiaires[0];
+    this.myAccount = myAccount;
+    const allItems: NodeListOf<Element> = window.document.querySelectorAll('div.consult-user');
+    for (let i = 0; i < allItems.length; i++) {
+      allItems[i].className = 'consult-user';
+    }
+    e.currentTarget.classList.add('active');
+    this.showReceiverInfo = true;
+  }
+
+
+  // public clearAmount() {this.amountToReceiver = undefined; }
+  public clearSearch() {
+    this.amountToReceiver = undefined;
+    this.newReceiver = new ReceiverClass('', '', '', '');
+    this.successMessage = '';
+    this.errorMessage = '';
+  }
+
 
 
 }
