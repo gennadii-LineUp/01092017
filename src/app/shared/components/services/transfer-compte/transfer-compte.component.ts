d2fb81e74f3eaf05fc229394b6e64311bf5f3a24
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-10-25 19:48:54 +0300

    page services/transfer-compte: found bug

--- src/app/shared/components/services/transfer-compte/transfer-compte.component.ts
+++ src/app/shared/components/services/transfer-compte/transfer-compte.component.ts
@@ -4,12 +4,13 @@ import {CommonServices} from '../../../../services/common.service';
 import {ErrorMessageHandlerService} from '../../../../services/error-message-handler.service';
 import {ReceiverClass} from '../../../../models/receiver-class';
 import {W2WVirementAccountService} from '../../../../services/api/W2WVirementAccount.service';
+import {GetCommissionsTTCService} from '../../../../services/api/getCommissionsTTC.service';
 
 @Component({
   selector: 'app-services-transfer-compte',
   templateUrl: './transfer-compte.component.html',
   styleUrls: ['./transfer-compte.component.scss'],
-  providers: [W2WVirementAccountService]
+  providers: [W2WVirementAccountService, GetCommissionsTTCService]
 
 })
 export class TransferCompteComponent implements OnInit {
@@ -23,19 +24,26 @@ export class TransferCompteComponent implements OnInit {
   loading = false;
   myAccount: any;
   account_id = '';
-  newReceiver = this.userDataService.beneficiaires[0];
+  // newReceiver = this.userDataService.beneficiaires[0];
+  newReceiver = new ReceiverClass('', '', '', '', undefined, '');
   amountToReceiver: number;
   showReceiverInfo = false;
   successMessage = '';
   errorMessage = '';
+  commission = [];
+  receivers = [new ReceiverClass('Tom', 'Henks', '123456789', '15', 18, 'citizen'),
+    new ReceiverClass('Ann', 'Hattaway', '+38(123)4567890', '2', 19, 'citizen'),
+    new ReceiverClass('Bon', 'Jovi', '12-345-67-89', '24', 20, 'citizen')];
 
 
   constructor(public userDataService: UserDataService,
               public commonServices: CommonServices,
               public w2WVirementAccountService: W2WVirementAccountService,
+              public getCommissionsTTCService: GetCommissionsTTCService,
               public errorMessageHandlerService: ErrorMessageHandlerService) { }
 
   ngOnInit() {
+    this.userDataService.setMyAccounts();
   }
 
   public goToAllAccountsFunction() {
@@ -81,28 +89,63 @@ export class TransferCompteComponent implements OnInit {
     // this.transfer_facture = true;
   }
 
+  public setBeneficiaryFunction(beneficiary: any) {
+    this.newReceiver = beneficiary;
+    console.log(this.newReceiver);
+  }
+
 
   public submitTransferCompteFunction() {
     this.loading = true;
     this.successMessage = '';
     this.errorMessage = '';
+    this.commission = [];
 
     console.log(this.myAccount);
     console.log(this.amountToReceiver);
     console.log(this.newReceiver);
-    this.w2WVirementAccountService.transferCompteStandart(this.amountToReceiver, this.myAccount.account_id, this.newReceiver.account_id)
+
+    this.getCommissionsTTCService.getCommission(this.amountToReceiver, 'W2W')
       .subscribe(result => {
-        this.loading = false;
         console.log(result._body);
         const response = this.commonServices.xmlResponseParcer_simple( result._body );
 
         console.dir( response );
         if (+response.error === 0) {
-          // this.showReceiverInfo = false;
-          // this.clearSearch();
-          // this.successMessage = response.message;
-          // this.discardReceiverInfoFunction();
+          this.errorMessage = response.message + ' - ' + response.commission;
+          this.commission.push(+response.commission);
+          console.log(this.commission);
+          /////////////////////////////
+          this.w2WVirementAccountService.transferCompteStandart(this.amountToReceiver,
+                                                                this.commission[0],
+                                                                this.myAccount.account_id,
+                                                                this.newReceiver.account_id)
+            .subscribe(_result => {
+              this.loading = false;
+              console.log(_result._body);
+              const _response = this.commonServices.xmlResponseParcer_simple( _result._body );
+
+              console.dir( _response );
+              if (+_response.error === 0) {
+                // this.showReceiverInfo = false;
+                // this.clearSearch();
+                // this.successMessage = response.message;
+                // this.discardReceiverInfoFunction();
+              } else {
+                this.errorMessage += '  ' + this.errorMessageHandlerService.getMessageEquivalent(_response.message);
+              }
+
+            }, (err) => {
+              this.loading = false;
+              console.log(err);
+              this.errorMessage = this.errorMessageHandlerService.getMessageEquivalent(err._body.type);
+              if (!this.errorMessage) {
+                this.errorMessage = this.errorMessageHandlerService._getMessageEquivalent(err._body);
+              }
+            });
+          /////////////////////////////
         } else {
+          this.loading = false;
           this.errorMessage = this.errorMessageHandlerService.getMessageEquivalent(response.message);
         }
 
@@ -110,9 +153,6 @@ export class TransferCompteComponent implements OnInit {
         this.loading = false;
         console.log(err);
         this.errorMessage = this.errorMessageHandlerService.getMessageEquivalent(err._body.type);
-        if (!this.errorMessage) {
-          this.errorMessage = this.errorMessageHandlerService._getMessageEquivalent(err._body);
-        }
       });
 
   }
@@ -121,8 +161,9 @@ export class TransferCompteComponent implements OnInit {
   public fillReceiverInfoFunction(myAccount: any, e: any) {
     // this.showReceiverInfo = false;
     this.clearSearch();
-    this.newReceiver = this.userDataService.beneficiaires[0];
+    this.newReceiver = new ReceiverClass('', '', '', '', undefined, '');
     this.myAccount = myAccount;
+    console.log(this.myAccount);
     const allItems: NodeListOf<Element> = window.document.querySelectorAll('div.consult-user');
     for (let i = 0; i < allItems.length; i++) {
       allItems[i].className = 'consult-user';
@@ -136,9 +177,10 @@ export class TransferCompteComponent implements OnInit {
   public clearAmount() {this.amountToReceiver = undefined; }
   public clearSearch() {
     this.amountToReceiver = undefined;
-    this.newReceiver = new ReceiverClass('', '', '', '', 0, '');
+    // this.newReceiver = new ReceiverClass('', '', '', '', undefined, '');
     this.successMessage = '';
     this.errorMessage = '';
+    this.commission = [];
   }
 
 
