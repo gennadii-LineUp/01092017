1932b5517b06cbee0b5631caf22817a814953941
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-11-07 15:09:03 +0200

    new services created: GetAllCitizenService, GetAllContractsService, GetAllCustomerService, ListPaysService

---
+++ src/app/services/api/getAllCitizen.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {BackendService} from '../backend.service';
+import {UrlParams} from '../../models/URL_PARAMS';
+
+@Injectable()
+export class GetAllCitizenService {
+
+  constructor(public backendService: BackendService) {}
+
+
+  public getAllCitizens(): Observable<any> {
+    const token = localStorage.token;
+
+    const body =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:run="http://runtime.services.cash.innov.sn/">
+         <soapenv:Header/>
+         <soapenv:Body>
+            <run:getAllCitizen>
+               </run:getAllCitizen>
+         </soapenv:Body>
+      </soapenv:Envelope>`;
+
+    console.log(body);
+    return this.backendService.post(UrlParams.backendUrl, body);
+  }
+
+
+}
---
+++ src/app/services/api/getAllCustomer.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {BackendService} from '../backend.service';
+import {UrlParams} from '../../models/URL_PARAMS';
+
+@Injectable()
+export class GetAllCustomerService {
+
+  constructor(public backendService: BackendService) {}
+
+
+  public getAllCustomer(): Observable<any> {
+    const token = localStorage.token;
+
+    const body =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:run="http://runtime.services.cash.innov.sn/">
+        <soapenv:Header/>
+          <soapenv:Body>
+              <run:getAllCustomer/>
+          </soapenv:Body>
+      </soapenv:Envelope>`;
+
+    console.log(body);
+    return this.backendService.post(UrlParams.backendUrl, body);
+  }
+
+
+}
