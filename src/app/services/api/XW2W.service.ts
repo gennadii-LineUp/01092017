2948acf7977a57cd86773098408b102d2b83ee9a
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-11-22 14:29:10 +0200

    'Paiements de masses reçus' added service

---
+++ src/app/services/api/XW2W.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {BackendService} from '../backend.service';
+import {UrlParams} from '../../models/URL_PARAMS';
+
+@Injectable()
+export class XW2WService {
+
+  constructor(public backendService: BackendService) {}
+
+
+  public paiementsRecus(accountId: string): Observable<any> {
+    const token = localStorage.token;
+
+    const body =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:run="http://runtime.services.cash.innov.sn/">
+         <soapenv:Header/>
+         <soapenv:Body>
+            <run:XW2W>
+               <sessionId>` + token + `</sessionId>
+               <accountId>` + accountId + `</accountId>
+            </run:XW2W>
+         </soapenv:Body>
+      </soapenv:Envelope>`;
+
+    console.log(body);
+    return this.backendService.post(UrlParams.backendUrl, body);
+  }
+
+
+}
--- src/app/shared/components/services/paiements/paiements.component.ts
+++ src/app/shared/components/services/paiements/paiements.component.ts
@@ -1,29 +1,50 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {UserDataService} from '../../../../models/user-data';
 import 'rxjs/add/operator/takeWhile';
+import {XW2WService} from '../../../../services/api/XW2W.service';
+import {ErrorMessageHandlerService} from '../../../../services/error-message-handler.service';
+import {GetAllListAccountService} from '../../../../services/api/getAllListAccount.service';
+import {CommonServices} from '../../../../services/common.service';
 
 @ Component({
   selector: 'app-services-paiements',
   templateUrl: 'paiements.component.html',
-  styleUrls: ['paiements.component.scss']
+  styleUrls: ['paiements.component.scss'],
+  providers: [XW2WService, GetAllListAccountService]
 })
 export class PaiementsComponent implements OnInit, OnDestroy {
+  errorMessage = '';
+  loading = true;
+  operations = [];
   alive = true;
 
-  constructor(public userDataService: UserDataService) { }
+  constructor(public userDataService: UserDataService,
+              public commonServices: CommonServices,
+              public getAllListAccountService: GetAllListAccountService,
+              public xW2WService: XW2WService,
+              public errorMessageHandlerService: ErrorMessageHandlerService) { }
 
   ngOnInit() {
     if ((this.userDataService.getMyAccounts()).length) {
-      console.log('=== MyAccounts\' length ' + this.userDataService.getMyAccounts().length);
+      this.paiementsRecusFunction((this.userDataService.getMyAccounts()['0']).id_account);
     } else {
-      console.log('=== MyAccounts\' is empty ===');
-      this.userDataService.setMyAccounts();
+      this.loading = true;
+      this.getAllListAccountService.getMyAccounts(localStorage.telephone)
+        .takeWhile(() => this.alive)
+        .subscribe(result => {
+          const response1 = this.commonServices.xmlResponseParcer_complex(result._body);
+          const accounts = response1.accounts;
+          if (accounts && accounts.length && accounts['0'].status === 'ACTIF') {
+           this.paiementsRecusFunction('' + accounts['0'].id);
+          }
+        }, err => {
+          this.loading = false;
+          this.errorMessage = this.errorMessageHandlerService.getMessageEquivalent(err._body.type);
+          if (!this.errorMessage) {
+            this.errorMessage = this.errorMessageHandlerService._getMessageEquivalent(err._body);
+          }
+        });
     }
-
-    const profil = ((<any>this.userDataService.getUser).profil) ? (<any>this.userDataService.getUser).profil :
-      localStorage.getItem('profil');
-    console.log(profil);
-    this.userDataService.setReceivers(profil);
   }
 
   ngOnDestroy() {
@@ -31,4 +52,26 @@ export class PaiementsComponent implements OnInit, OnDestroy {
   }
 
 
+  public paiementsRecusFunction(accountId: string) {
+    this.loading = true;
+    this.operations = [];
+    this.xW2WService.paiementsRecus(accountId)
+      .takeWhile(() => this.alive)
+      .subscribe(result => {
+        this.loading = false;
+        const response = this.commonServices.xmlResponseParcer_complex(result._body);
+        console.log(response);
+       if (+response.error === 0) {
+         this.operations = response.operation;
+       }
+        this.userDataService.setMyAccounts();
+      }, err => {
+        console.log(err);
+        this.loading = false;
+        this.errorMessage = this.errorMessageHandlerService.getMessageEquivalent(err._body.type);
+        if (!this.errorMessage) {
+          this.errorMessage = this.errorMessageHandlerService._getMessageEquivalent(err._body);
+        }
+      });
+  }
 }
