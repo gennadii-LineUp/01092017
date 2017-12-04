2489c5bdcd342c7bd624e78708c28a3c94e67eed
gennadii-LineUp <gennadii.r@it-lineup.com>
2017-12-04 15:23:55 +0200

    added 'back'-button for mobile at pages:  manage-admin-citizen

--- src/app/shared/components/services/manage-admin-citizen/manage-admin-citizen.component.ts
+++ src/app/shared/components/services/manage-admin-citizen/manage-admin-citizen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-services-manage-admin-citizen',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-admin-citizen.component.scss']
 })
 export class ManageAdminCitizenComponent implements OnInit {
+  userRole = '';
+  alive = true;
 
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activateRoute.parent.url
+      .takeWhile(() => this.alive)
+      .subscribe(resp =>  this.userRole = resp['0'].path);
   }
 
 }
