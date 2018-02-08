f2b9d7352c9d590cf76f97a656d717ef81bedc54
genarybin <genarybin1979@gmail.com>
2018-02-08 11:10:18 +0200

    added services GetCustomerBycellular and GetContactOfCustomer for Depot Client

---
+++ src/app/services/api/getContactOfCustomer.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {BackendService} from '../backend.service';
+import {UrlParams} from '../../models/URL_PARAMS';
+
+@Injectable()
+export class GetContactOfCustomerService {
+
+  constructor(public backendService: BackendService) {}
+
+
+  public getContactOfCustomer(uoId: string): Observable<any> {
+    const token = localStorage.token;
+
+    const body =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:run="http://runtime.services.cash.innov.sn/">
+         <soapenv:Header/>
+         <soapenv:Body>
+            <run:getContactOfCustomer>
+               <idSession>` + token + `</idSession>
+               <uoId>` + uoId + `</uoId>
+            </run:getContactOfCustomer>
+         </soapenv:Body>
+      </soapenv:Envelope>`;
+
+    console.log(body);
+    return this.backendService.post(UrlParams.backendUrl, body);
+  }
+
+}
---
+++ src/app/services/api/getCustomerByCellular.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {BackendService} from '../backend.service';
+import {UrlParams} from '../../models/URL_PARAMS';
+
+@Injectable()
+export class GetCustomerByCellularService {
+
+  constructor(public backendService: BackendService) {}
+
+
+  public getCustomerByCellular(cellular: string): Observable<any> {
+    const token = localStorage.token;
+
+    const body =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:run="http://runtime.services.cash.innov.sn/">
+         <soapenv:Header/>
+         <soapenv:Body>
+            <run:getCustomerByCellular>
+               <idSession>` + token + `</idSession>
+               <cellular>` + cellular + `</cellular>
+            </run:getCustomerByCellular>
+         </soapenv:Body>
+      </soapenv:Envelope>`;
+
+    console.log(body);
+    return this.backendService.post(UrlParams.backendUrl, body);
+  }
+
+}
