7ac3ac2c16ef9929bb5e2adac02cbeb57b5c8bae
genarybin <genarybin1979@gmail.com>
2018-03-14 16:44:15 +0200

    notification-page: added LireNotif (for reading)

---
+++ src/app/services/api/lireNotif.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {BackendService} from '../backend.service';
+import {UrlParams} from '../../models/URL_PARAMS';
+
+@Injectable()
+export class LireNotifService {
+
+  constructor(public backendService: BackendService) {}
+
+
+  public lireNotif(notifId: string): Observable<any> {
+    const token = localStorage.token;
+
+    const body =
+      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:run="http://runtime.services.cash.innov.sn/">
+         <soapenv:Header/>
+         <soapenv:Body>
+            <run:lireNotif>
+               <idSession>` + token + `</idSession>
+               <notifId>` + notifId + `</notifId>
+            </run:lireNotif>
+         </soapenv:Body>
+      </soapenv:Envelope>`;
+
+    console.log(body);
+    return this.backendService.post(UrlParams.backendUrl, body);
+  }
+
+
+}
--- src/app/shared/components/notifications/notifications.component.ts
+++ src/app/shared/components/notifications/notifications.component.ts
@@ -6,12 +6,13 @@ import {CurrencyParams} from '../../../models/currency_params';
 import {CommonServices} from '../../../services/common.service';
 import {GetAllNotifService} from '../../../services/api/getAllNotif.service';
 import 'rxjs/add/operator/takeWhile';
+import {LireNotifService} from '../../../services/api/lireNotif.service';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss'],
-  providers: [GetAllNotifService]
+  providers: [GetAllNotifService, LireNotifService]
 })
 export class NotificationsComponent implements OnInit, OnDestroy {
   userRole = '';
@@ -29,11 +30,12 @@ export class NotificationsComponent implements OnInit, OnDestroy {
               public getAllNotifService: GetAllNotifService,
               public errorMessageHandlerService: ErrorMessageHandlerService,
               private activateRoute: ActivatedRoute,
-              public currencyParams: CurrencyParams) {
+              public currencyParams: CurrencyParams,
+              public lireNotifService: LireNotifService) {
     userDataService.myAccounts$.subscribe((myAccounts) => {
       console.log(myAccounts);
       console.log('hello');
-      this.loadNotifications(this.userDataService.getMyAccounts()['0'].uoId);
+      this.loadNotificationsFunction(this.userDataService.getMyAccounts()['0'].uoId);
     });
   }
 
@@ -44,7 +46,7 @@ export class NotificationsComponent implements OnInit, OnDestroy {
 
     if ((this.userDataService.getMyAccounts()).length) {
       // console.log('=== MyAccounts\' length ' + this.userDataService.getMyAccounts().length);
-      this.loadNotifications(this.userDataService.getMyAccounts()['0'].uoId);
+      this.loadNotificationsFunction(this.userDataService.getMyAccounts()['0'].uoId);
     } else {
       console.log('=== MyAccounts\' is empty ===');
       this.userDataService.setMyAccounts();
@@ -53,16 +55,13 @@ export class NotificationsComponent implements OnInit, OnDestroy {
     this.profil = ((<any>this.userDataService.getUser).profil) ? (<any>this.userDataService.getUser).profil :
       localStorage.getItem('profil');
     console.log(this.profil);
-
-    // this.loadNotifications();
-
   }
 
   ngOnDestroy() {
     this.alive = false;
   }
 
-  loadNotifications(uoId: string) {
+  loadNotificationsFunction(uoId: string) {
     this.loading = true;
     this.notifications = [];
     this.getAllNotifService.getAllNotif(uoId)
@@ -107,8 +106,39 @@ export class NotificationsComponent implements OnInit, OnDestroy {
     }
   }
 
-  public isShown(lu: string): boolean {
-    return (lu === 'lu') ? true : false;
+  public isNonLu(lu: string): boolean {
+    return (lu === 'lu') ? false : true;
   }
 
+  markNotificationAsLu(id: string) {
+    console.log(id);
+    const notif_element = window.document.getElementById(id);
+    if (notif_element && notif_element.classList && notif_element.classList.contains('non-lu')) {
+      notif_element.classList.remove('non-lu');
+    }
+    this.LireNotifFunction(id);
+  }
+
+  LireNotifFunction(notifId: string) {
+    this.loading = true;
+    this.lireNotifService.lireNotif(notifId)
+      .takeWhile(() => this.alive)
+      .subscribe(result => {
+        this.loading = false;
+        console.log(result);
+        const notifications = (this.commonServices.xmlResponseParcer_complex(result._body)); //
+        // if (notifications.length) {
+        //   this.notifications = (notifications.length) ? notifications : [];
+        //   this._notifications = this.notifications;
+        // } else {
+        //   this.errorMessage = 'Error: ' + notifications.message.toLowerCase();
+        // }
+        console.log(notifications);
+      }, (err) => {
+        this.loading = false;
+        console.log(err);
+        this.errorMessage = this.errorMessageHandlerService.getMessageEquivalent(err._body.type);
+      });
+
+  }
 }
