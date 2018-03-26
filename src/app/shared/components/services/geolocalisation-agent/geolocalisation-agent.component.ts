4f62836a539c38c70c343183eb0603a4220ceb22
genarybin <genarybin1979@gmail.com>
2018-03-26 18:27:56 +0300

    geolocalisation-page: started work with geolocation-plugin

--- src/app/shared/components/services/geolocalisation-agent/geolocalisation-agent.component.ts
+++ src/app/shared/components/services/geolocalisation-agent/geolocalisation-agent.component.ts
@@ -2,6 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {MarkerClass} from '../../../../models/marker-class';
 
+declare var navigator: any;
+
 @Component({
   selector: 'app-services-geolocalisation-agent',
   templateUrl: './geolocalisation-agent.component.html',
@@ -31,5 +33,32 @@ export class GeolocalisationAgentComponent implements OnInit, OnDestroy {
 
   public startGettingMyCoord() {
     console.log('startGettingMyCoord');
+
+    // cordova.plugins.barcodeScanner.scan(
+    //   (result) => {
+    //     const s = 'Result: ' + result.text + '<br/>';
+    //     console.log(result.text);
+    //     setTimeout(this.showQRdata(result.text), 100);
+    //   },
+    //   function (error) {
+    //     alert('Scanning failed: ' + error);
+    //   }
+    // );
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
+                            'Longitude: ' + position.coords.longitude     + '<br />'; //  +
+                            // '<hr />'      + element.innerHTML;
+
+
+        // const s = 'Result: ' + position.text + '<br/>';
+        console.log(position);
+        // setTimeout(this.showQRdata(result.text), 100);
+      },
+      (error) => {
+        alert('Scanning failed: ' + error);
+      },
+      []);
   }
 }
